0d7818e5d29f80676a64c9fdb622fc08a16e8b88
Eryk Tavares <EricTavares35@gmail.com>
2026-04-09 22:31:57 -0300

    feat(expenses): add initial list of registered expenses

--- lib/screens/expenses.dart
+++ lib/screens/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,6 +11,33 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Japão',
+      amount: 3500,
+      category: Category.travel,
+      date: DateTime(2026, 7, 15),
+    ),
+    Expense(
+      title: 'One piece Stampede',
+      amount: 18.99,
+      category: Category.leisure,
+      date: DateTime.now(),
+    ),
+    Expense(
+      title: 'Home Renovation',
+      amount: 5370,
+      category: Category.work,
+      date: DateTime(2026, 5, 05),
+    ),
+    Expense(
+      title: 'Shrimp bean ball',
+      amount: 22.50,
+      category: Category.food,
+      date: DateTime(2026, 4, 23),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
