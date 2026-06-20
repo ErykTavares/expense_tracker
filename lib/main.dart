c1013324e9ec9b8a27f1d340e0316515d0080e52
Eryk Tavares <EricTavares35@gmail.com>
2026-06-19 22:18:47 -0300

    feat(main): enhance theme configuration for Material 3

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,31 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  ColorScheme? get colorScheme => null;
+  final primaryColor = const Color(0xffd32f2f);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme(
+          brightness: Brightness.light,
+          primary: primaryColor,
+          onPrimary: Colors.white, // Text color on top of primary red
+          secondary: Color(0xFFFF5252), // Bright Coral Red for accents
+          onSecondary: Colors.white,
+          error: Color(0xFFB00020), // Error Red
+          onError: Colors.white,
+          surface: Color(
+            0xFFF5F5F5,
+          ), // Very faint pinkish-white tint background
+          onSurface: Colors.grey[900]!,
+          // Dark gray for text on surface
+        ),
+      ),
       home: Expenses(),
     );
   }
