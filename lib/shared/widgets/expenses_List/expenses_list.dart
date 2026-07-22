8fb5c4436fc0d1beba9c594fc2eba6608a7b49f4
Eryk Tavares <EricTavares35@gmail.com>
2026-07-22 01:01:47 -0300

    feat(expenses): implement dismissible functionality for expense removal

--- lib/shared/widgets/expenses_List/expenses_list.dart
+++ lib/shared/widgets/expenses_List/expenses_list.dart
@@ -3,12 +3,25 @@ import 'package:expense_tracker/shared/widgets/expenses_List/expenses_item.dart'
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+    required this.onRemoveExpense,
+  });
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
-  Widget listViewBuilder(BuildContext ctx, int index) =>
-      ExpensesItem(expense: expenses[index]);
+  Widget listViewBuilder(BuildContext ctx, int index) => Dismissible(
+    onDismissed: (direction) {
+      if (direction == DismissDirection.startToEnd) {
+        onRemoveExpense(expenses[index]);
+      }
+    },
+    direction: DismissDirection.startToEnd,
+    key: ValueKey(expenses[index]),
+    child: ExpensesItem(expense: expenses[index]),
+  );
 
   @override
   Widget build(BuildContext context) {
